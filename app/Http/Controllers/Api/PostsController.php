3c0bc2e0522cd28e0d53ac7cabdb9bc9d0cfcf75
vagrant <vagrant@homestead>
2020-08-30 09:33:36 +0000

    图片a连接，内容显示分类

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use TCG\Voyager\Models\Post;
+use TCG\Voyager\Models\Category;
 use Parsedown;
 
 class PostsController extends Controller
@@ -18,10 +19,11 @@ class PostsController extends Controller
         return response()->json($posts);
     }
 
-    public function show(Parsedown $parsedown, Post $post)
+    public function show(Parsedown $parsedown, Post $post, Category $category)
     {
 
         $post->body = $parsedown->text($post->body);
+        $post->category = $category->find($post->category_id)->name;
         return response()->json($post);
     }
 
